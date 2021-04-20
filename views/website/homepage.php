8a94a51e162c6890569983fe6c4de8db834ea69d
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-04-20 20:55:36 -0300

    🔄️ Refactor homepage

--- views/website/homepage.php
+++ views/website/homepage.php
@@ -4,31 +4,29 @@
 ?>
 
 
-<section>
-    <article>
-        <div class="container__image-profile">
-            <img src=<?php echo "/assets/images/$profileImageFilename" ?> alt="Gabriel Santos - Imagem de perfil">
-        </div>
-        
-        <div class="container__instagram-title">
-            <a href=<?php echo "https://instagram.com/$profileInstagram" ?> ><?php echo "@$profileInstagram"; ?></a>
-        </div>
+<article id="container__profile">
+    <div class="container__image-profile">
+        <img src=<?php echo "/assets/images/$profileImageFilename" ?> alt="Gabriel Santos - Imagem de perfil">
+    </div>
+    
+    <div class="container__instagram-title">
+        <a href=<?php echo "https://instagram.com/$profileInstagram" ?> ><?php echo "@$profileInstagram"; ?></a>
+    </div>
 
-        <div class="group__buttons-cta">
-            <?php 
-                $buttonTemplate = <<<Template
-                    <a href="%buttonHref">%buttonTitle</a>
-                Template;
+    <div class="group__buttons-cta">
+        <?php 
+            $buttonTemplate = <<<Template
+                <a class="button button-item" href="%buttonHref">%buttonTitle</a>
+            Template;
 
-                foreach ($this->language['buttons'] as $button) {
-                    $currentHtmlButton = $buttonTemplate;
+            foreach ($this->language['buttons'] as $button) {
+                $currentHtmlButton = $buttonTemplate;
 
-                    $currentHtmlButton = str_replace('%buttonTitle', $button['title'], $currentHtmlButton);
-                    $currentHtmlButton = str_replace('%buttonHref', $button['link'], $currentHtmlButton);
+                $currentHtmlButton = str_replace('%buttonTitle', $button['title'], $currentHtmlButton);
+                $currentHtmlButton = str_replace('%buttonHref', $button['link'], $currentHtmlButton);
 
-                    echo $currentHtmlButton;
-                }
-            ?>
-        </div>
-    </article>
-</section>
+                echo $currentHtmlButton;
+            }
+        ?>
+    </div>
+</article>
