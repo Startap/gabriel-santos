f30225ae83ff38469540f477eddcf17f7a12d9d3
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-04-20 19:53:15 -0300

    📃 Add homepage html

--- views/website/homepage.php
+++ views/website/homepage.php
@@ -1,1 +1,34 @@
+<?php 
+    $profileImageFilename = $this->language['profile']['image_profile'];
+    $profileInstagram = $this->language['profile']['instagram_title']
+?>
 
+
+<section>
+    <article>
+        <div class="container__image-profile">
+            <img src=<?php echo "/assets/images/$profileImageFilename" ?> alt="Gabriel Santos - Imagem de perfil">
+        </div>
+        
+        <div class="container__instagram-title">
+            <a href=<?php echo "https://instagram.com/$profileInstagram" ?> ><?php echo "@$profileInstagram"; ?></a>
+        </div>
+
+        <div class="group__buttons-cta">
+            <?php 
+                $buttonTemplate = <<<Template
+                    <a href="%buttonHref">%buttonTitle</a>
+                Template;
+
+                foreach ($this->language['buttons'] as $button) {
+                    $currentHtmlButton = $buttonTemplate;
+
+                    $currentHtmlButton = str_replace('%buttonTitle', $button['title'], $currentHtmlButton);
+                    $currentHtmlButton = str_replace('%buttonHref', $button['link'], $currentHtmlButton);
+
+                    echo $currentHtmlButton;
+                }
+            ?>
+        </div>
+    </article>
+</section>
