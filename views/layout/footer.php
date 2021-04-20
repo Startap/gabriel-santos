4e07f3acfb71037022ffa34533583d91aada17fd
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-04-20 20:55:11 -0300

    ©️ Include copyright text

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,9 +1,14 @@
 <footer>
-    Copyright <?php echo date('Y'); ?> | Todos os direitos reservados
+    <p>©️ <?php echo date('Y'); ?> - <b>Gabriel Santos Freitas</b></p>
+    <p>Todos os direitos reservados</p>
+
+
+
 </footer>
 
 %scripts_template%
 
 <script type="text/javascript" src="/assets/dist/app.js"></script>
 </body>
+
 </html>
