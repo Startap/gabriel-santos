71eeb133c4183b972ce15e687e1b6403e06b3d5f
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-04-20 20:55:22 -0300

    🔤 Language chanegs

--- components/Language.php
+++ components/Language.php
@@ -7,9 +7,9 @@
 
         'buttons' => [
             ['title' => 'Titãs Digitais', 'link' => 'https://www.titansdigitais.com.br/'],
-            ['title' => 'Telegram - Conteúdos Exclusivos', 'link' => 'https://t.me/gabrielsantosf'],
+            ['title' => 'Telegram <br>Conteúdos Exclusivos', 'link' => 'https://t.me/gabrielsantosf'],
             ['title' => 'YouTube', 'link' => 'https://www.youtube.com/channel/UCLIrhUNhandg8fwd8NpGSXQ'],
-            ['title' => 'Semana LANCE SEU CURSO DO ABSOLUTO ZERO', 'link' => 'https://www.lanceseucurso.com.br/'],
+            ['title' => 'Semana Lance seu curso<br> do absoluto zero', 'link' => 'https://www.lanceseucurso.com.br/'],
             ['title' => 'Agência New Times', 'link' => 'https://agencianewtimes.com.br/'],
         ]
     ];  
