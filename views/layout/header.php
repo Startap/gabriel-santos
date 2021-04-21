85a678c40ce42070630ad124e439dcf5635064b3
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-04-21 11:18:17 -0300

    🖼️ Include favicon reference on header

--- views/layout/header.php
+++ views/layout/header.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <meta name="description" content="%APP_DESCRIPTION%">
     <meta name="keywords" content="%APP_KEYWORDS%">
@@ -14,6 +14,10 @@
     <link rel="stylesheet" href="/assets/dist/reset.css">
     <link rel="stylesheet" href="/assets/dist/app.css">
 
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/image/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/image/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/image/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/assets/image/favicon/site.webmanifest">
 </head>
 
 <body>
