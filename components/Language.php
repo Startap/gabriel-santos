838bb5bef20da127690ca5bcd1c5e05fa18c7ed5
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-04-20 21:46:03 -0300

    🚧 Finish release to customer review

--- components/Language.php
+++ components/Language.php
@@ -7,9 +7,9 @@
 
         'buttons' => [
             ['title' => 'Titãs Digitais', 'link' => 'https://www.titansdigitais.com.br/'],
-            ['title' => 'Telegram <br>Conteúdos Exclusivos', 'link' => 'https://t.me/gabrielsantosf'],
+            ['title' => 'Telegram <br><small>Conteúdos Exclusivos</small>', 'link' => 'https://t.me/gabrielsantosf'],
             ['title' => 'YouTube', 'link' => 'https://www.youtube.com/channel/UCLIrhUNhandg8fwd8NpGSXQ'],
             ['title' => 'Semana Lance seu curso<br> do absoluto zero', 'link' => 'https://www.lanceseucurso.com.br/'],
             ['title' => 'Agência New Times', 'link' => 'https://agencianewtimes.com.br/'],
-        ]
+        ],
     ];  
--- views/website/homepage.php
+++ views/website/homepage.php
@@ -1,32 +1,45 @@
-<?php 
-    $profileImageFilename = $this->language['profile']['image_profile'];
-    $profileInstagram = $this->language['profile']['instagram_title']
+<?php
+$profileImageFilename = $this->language['profile']['image_profile'];
+$profileInstagram = $this->language['profile']['instagram_title']
 ?>
 
 
-<article id="container__profile">
-    <div class="container__image-profile">
+<section id="container__profile">
+    <section class="container__image-profile">
         <img src=<?php echo "/assets/images/$profileImageFilename" ?> alt="Gabriel Santos - Imagem de perfil">
-    </div>
-    
-    <div class="container__instagram-title">
-        <a href=<?php echo "https://instagram.com/$profileInstagram" ?> ><?php echo "@$profileInstagram"; ?></a>
-    </div>
+    </section>
 
-    <div class="group__buttons-cta">
-        <?php 
-            $buttonTemplate = <<<Template
-                <a class="button button-item" href="%buttonHref">%buttonTitle</a>
-            Template;
+    <section class="container__instagram-title">
+        <?php
+        $instagramTemplate = <<<HtmlInstagram
+        <a href='https://instagram.com/%profileInstagram'>
+            <span class="iconify" data-icon="bi-instagram"></span>
+            <span>%profileInstagram</span>
+        </a>
+        HtmlInstagram;
 
-            foreach ($this->language['buttons'] as $button) {
-                $currentHtmlButton = $buttonTemplate;
+        $instagramTemplate = str_replace('%profileInstagram', $profileInstagram, $instagramTemplate);
 
-                $currentHtmlButton = str_replace('%buttonTitle', $button['title'], $currentHtmlButton);
-                $currentHtmlButton = str_replace('%buttonHref', $button['link'], $currentHtmlButton);
-
-                echo $currentHtmlButton;
-            }
+        echo $instagramTemplate;        
         ?>
-    </div>
-</article>
+        </section>
+
+    <section class="group__buttons-cta">
+        <?php
+        $buttonTemplate = <<<Template
+            <a class="button button-item" target="_blank" href="%buttonHref">
+                <span>%buttonTitle</span>
+            </a>
+        Template;
+
+        foreach ($this->language['buttons'] as $button) {
+            $currentHtmlButton = $buttonTemplate;
+
+            $currentHtmlButton = str_replace('%buttonTitle', $button['title'], $currentHtmlButton);
+            $currentHtmlButton = str_replace('%buttonHref', $button['link'], $currentHtmlButton);
+
+            echo $currentHtmlButton;
+        }
+        ?>
+    </section>
+</section>
